8e6a76c7f16656b0fe46c9b2e897ace76d9dc357
Ronald Oswaldo Macal de León <58499613+ronaldmacal@users.noreply.github.com>
2021-06-07 01:59:40 -0600

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'crear-matriz',
+    loadChildren: () => import('./Paginas/crear-matriz/crear-matriz.module').then( m => m.CrearMatrizPageModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./Paginas/menu/menu.module').then( m => m.MenuPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./Paginas/login/login.module').then( m => m.LoginPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
